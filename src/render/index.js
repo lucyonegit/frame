f9828442700720f03e260324b6a1d79c94db6e5e
hanlujie <lucyonegit@gmail.com>
2023-11-24 08:46:39 +0800

    fix: bugs

--- src/render/index.js
+++ src/render/index.js
@@ -8,23 +8,25 @@ class FrameBar {
     this.dpr = window.devicePixelRatio;
     this.canvasCtx = null;
     this.rAFId = null
+    this.close = ()=>{}
   }
   _getContainer = () => {
     let container = this.containerId
       ? document.querySelector(`#${this.containerId}`)
       : document.body;
-    while (container.hasChildNodes()) {
-      container.removeChild(container.firstChild);
-    }
     return container;
   };
   init = () => {
-    let { clientWidth, clientHeight } = this._getContainer();
-    this.width = clientWidth;
-    this.height = clientHeight;
     this._initCanvasCtx();
   };
   _initCanvasCtx = () => {
+    let container = this._getContainer();
+    let { clientWidth, clientHeight } = container
+    while (container.hasChildNodes()) {
+      container.removeChild(container.firstChild);
+    }
+    this.width = clientWidth;
+    this.height = clientHeight;
     let canvas = document.createElement("canvas");
     canvas.width = this.width * this.dpr;
     canvas.height = this.height * this.dpr;
@@ -34,12 +36,11 @@ class FrameBar {
     context.width = this.width * this.dpr;
     context.height = this.height * this.dpr;
     context.scale(this.dpr, this.dpr);
-    this._getContainer().appendChild(canvas);
+    container.appendChild(canvas);
     this.canvasCtx = context;
   };
 
   _getDesktopAudioMediaStream = async () => {
-
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: false,
@@ -50,15 +51,13 @@ class FrameBar {
    *
    * @param {MediaStream} stream
    */
-  // 屏幕画面
   _createAnalyser = (stream) => {
-
     const audioCtx = new AudioContext();
     const source = audioCtx.createMediaStreamSource(stream);
     const analyser = audioCtx.createAnalyser();
     source.connect(analyser);
-    analyser.connect(audioCtx.destination);
-    analyser.fftSize = 128;
+    // analyser.connect(audioCtx.destination);
+    analyser.fftSize = 512;
     const getData = () => {
       let dataArray = new Uint8Array(analyser.frequencyBinCount);
       analyser.getByteFrequencyData(dataArray);
@@ -83,14 +82,14 @@ class FrameBar {
       let bar_x = i * bar_w;
       let bar_h = (data[i] / 255) * this.canvasCtx.height;
       let bar_y = this.canvasCtx.height - bar_h;
-      this.canvasCtx.fillStyle = `#000`;
+      let hue = i / data.length * 360
+      this.canvasCtx.fillStyle =  `hsl(${hue},50%,50%)` //`#5A3`;
       this.canvasCtx.fillRect(
         bar_x / this.dpr,
-        bar_y / this.dpr,
+        bar_y / this.dpr / 2,
         bar_w / this.dpr,
         bar_h / this.dpr
       );
-      this.canvasCtx.fillText(i + 1 + "", bar_x / this.dpr, 100);
       this.canvasCtx.fillStyle = "#0f0";
       if (this.maxHeight[i]) {
         if (this.maxHeight[i] < bar_y / this.dpr) {
@@ -114,9 +113,10 @@ class FrameBar {
   run = async () => {
     this.init();
     const mediaStream = await this._getDesktopAudioMediaStream();
-    this._createAnalyser(mediaStream);
+    this.close = this._createAnalyser(mediaStream);
   };
   reset = () => {
+    this.close();
     cancelAnimationFrame(this.rAFId);
     this.init();
   };
